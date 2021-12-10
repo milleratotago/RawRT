6ea23e8607ccf64627f4a24333a0dc84e3e26d16
milleratotago <miller@psy.otago.ac.nz>
2021-12-10 15:36:05 +1300

    Update CondNs.m
    CondNs can now compute proportions

--- CondNs.m
+++ CondNs.m
@@ -2,6 +2,8 @@ function [outResultTable, outDVName] = CondNs(inTrials,CondSpecs,varargin)
 %
 % Include/Exclude options passed through to SubTableIndices.
 
+[wantProportions, varargin] = ExtractNamei({'p','proportions'},varargin);
+
 % [inTrials, varargin] = MaybeSelect(inTrials,varargin{:})
 
 [mySubTableIndices, outResultTable] = SubTableIndices(inTrials,CondSpecs,varargin{:});
@@ -9,6 +11,11 @@ function [outResultTable, outDVName] = CondNs(inTrials,CondSpecs,varargin)
 outDVName = UniqueVarname(outResultTable,'N');
 
 outResultTable.(outDVName) = cellfun(@numel,mySubTableIndices);
+
+if wantProportions
+    ttl = sum(outResultTable.(outDVName));
+    outResultTable.(outDVName) = outResultTable.(outDVName) / ttl;
+end
 
 outDVName = {outDVName};
 
